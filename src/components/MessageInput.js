530873e835403a524acb047fbb106ea2c05e7677
mmythily <mythily.mudunuru@gmail.com>
2020-02-13 21:43:07 -0500

    message board component architecture

---
+++ src/components/MessageInput.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+
+
+export default function MessageInput () {
+  const [message, setMessage] = useState('')
+
+  function handleChange(event) {
+    setMessage(event.target.value)
+  }
+  console.log(message)
+
+  return (
+    <div className='message-input'>
+      <input 
+        placeholder="Enter your Message here"
+        value={message}
+        type='text'
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
---
+++ src/components/MessagesList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Message from './Message'
+
+
+export default function MessagesList () {
+  return (
+    <div className='message-list'>
+      MessagesList
+      <Message />
+    </div>
+  )
+}
